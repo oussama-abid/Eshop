22993ccc3991e4c0bd4e90780fc7dd49aa389633
Eyüphan Bayram <brooo2002y@gmail.com>
2024-04-25 19:42:58 +0200

    Jungs ich weis nicht ob das funktionieren wird, ich versuche das mal

---
+++ src/Entities/Warenkorb.java
@@ -0,0 +1,33 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warenkorb {
+    private List<WarenkorbArtikel> items = new ArrayList<>();
+
+
+    public Warenkorb(List<WarenkorbArtikel> items) {
+        super();
+        this.items = items;
+    }
+
+    public List<WarenkorbArtikel> getItems() {
+        return items;
+    }
+
+    public void setItems(List<WarenkorbArtikel> items) {
+        this.items = items;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Entities/WarenkorbArtikel.java
@@ -0,0 +1,26 @@
+package Entities;
+
+public class WarenkorbArtikel {
+    private Artikel Artikel;
+    private int Anzahl;
+
+    public WarenkorbArtikel(Artikel artikel, int anzahl) {
+        super();
+        this.Artikel = artikel;
+        this.Anzahl = anzahl;
+    }
+
+
+    public Artikel getArtikel() {
+        return Artikel;
+    }
+    public void setArtikel(Artikel artikel) {
+        this.Artikel = artikel;
+    }
+    public int getAnzahl() {
+        return Anzahl;
+    }
+    public void setAnzahl(int anzahl) {this.Anzahl = anzahl;
+    }
+
+}
