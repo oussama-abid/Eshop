9c6301ea519967b6dec931a4ba7df966441a75b0
oussama-abid <oussamaabid52@gmail.com>
2024-05-29 12:55:18 +0200

    .

---
+++ src/Exceptions/Plzexception.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+
+    public class Plzexception extends Exception {
+        public Plzexception() {
+            super("Bitte geben Sie eine gültige plz ein.");
+        }
+    }
+
